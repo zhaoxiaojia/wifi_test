07734e07e5be078df45b76b434b8433090553b8a
zhaoxiaojia <799833928@qq.com>
2023-08-16 11:05:39 +0800

    fix db_set none

--- test/test_rvr.py
+++ test/test_rvr.py
@@ -307,7 +307,7 @@ pair_count = {
 }
 
 
-def set_pair_count(router_info, rssi_num, type, dire):
+def set_pair_count(router_info, db_set, type, dire):
     '''
     匹配 打流通道数
     '''
@@ -332,8 +332,8 @@ def set_pair_count(router_info, rssi_num, type, dire):
         target_list = _2g
     else:
         target_list = _5g
-    logging.info(f'rf current db {rssi_num}')
-    pair = pair_count[type][band][dire][bisect.bisect(target_list, rssi_num)]
+    logging.info(f'rf current db {db_set}')
+    pair = pair_count[type][band][dire][bisect.bisect(target_list, db_set)]
     logging.info(f'pair {pair}')
     return pair
 
@@ -520,6 +520,8 @@ def test_wifi_rvr(wifi_setup_teardown, rf_value):
         if 'rf' in env_control:
             db_set = rf_value[1] if type(rf_value) == tuple else rf_value
             info += 'db_set : ' + str(db_set) + '\n'
+        else:
+            db_set = 0
         if 'corner' in env_control:
             corner_set = rf_value[0] if type(rf_value) == tuple else rf_value
             info += 'corner_set : ' + str(corner_set) + '\n'
