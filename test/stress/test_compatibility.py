4a373f4b5243d89685b51945d4e9f989256bacf1
zhaoxiaojia <799833928@qq.com>
2025-02-18 11:03:55 +0800

    rssi 问题debug

--- test/stress/test_compatibility.py
+++ test/stress/test_compatibility.py
@@ -24,14 +24,16 @@ router_2g = Router(band='2.4 GHz', wireless_mode='11ac', channel='1', authentica
 router_5g = Router(band='5 GHz', wireless_mode='11ac', channel='36', authentication_method='Open System',
                    bandwidth='20 MHz', ssid=ssid_5g, wpa_passwd=passwd, expected_rate='10 10')
 
-with open(f"{os.getcwd()}/config/compatobility_expectdata.json", 'r') as f:
-    router_datas = json.load(f)
 
 
-def handle_expectdata(ip, port, band, mimo, dir):
+def handle_expectdata(ip, port, band, bandwidth, dir):
+    with open(f"compatobility_expectdata.json", 'r') as f:
+        router_datas = json.load(f)
     for data in router_datas:
         if data['ip'] == ip and data['port'] == port:
-            return data['data'][band][fpga[dut_fpgs][band]]['40MHz'][fpga[dut_fpgs][mimo]][dir]
+            print(data['data'][band][fpga[dut_fpgs][band]][bandwidth])
+            print([fpga[dut_fpgs]['mimo']])
+            return data['data'][band][fpga[dut_fpgs][band]][bandwidth][fpga[dut_fpgs]['mimo']][dir]
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -57,14 +59,14 @@ def router_setting(power_setting, request):
     logging.info(f'pc_ip {pc_ip}')
     router_set = power_setting[0]['data']
     band = request.param
-    expect_tx = handle_expectdata(router_set.ip, router_set.port, router_set.band, router_set.mimo, 'UL')
-    expect_rx = handle_expectdata(router_set.ip, router_set.port, router_set.band, router_set.mimo, 'DL')
+    expect_tx = handle_expectdata(router_set.ip, router_set.port, band, router_set.bandwidth, 'UL')
+    expect_rx = handle_expectdata(router_set.ip, router_set.port, band, router_set.bandwidth, 'DL')
     router = Router(band=band, wireless_mode=router_set.mode, channel='default', authentication_method='default',
                     bandwidth=router_set.bandwidth, ssid=ssid_2g, wpa_passwd=passwd,
                     expected_rate=f'{expect_tx} {expect_rx}')
     yield router
 
-
+@pytest.mark.dependency(name="scan")
 def test_scan(router_setting):
     pytest.dut.push_iperf()
     for _ in range(5):
@@ -76,13 +78,13 @@ def test_scan(router_setting):
     else:
         assert False, f"Can't scan target ssid {router.ssid}"
 
-
+@pytest.mark.dependency(name="connect",depends=["scan"])
 def test_connect(router_setting):
     pytest.dut.forget_wifi()
     pytest.dut.checkoutput(pytest.dut.get_wifi_cmd(router))
     assert pytest.dut.wait_for_wifi_address()[0], "Can't connect ssid"
 
-
+@pytest.mark.dependency(depends=["connect"])
 @pytest.mark.wifi_connect
 def test_multi_throughtput_tx(router_setting):
     router_info = router_setting
@@ -96,7 +98,7 @@ def test_multi_throughtput_tx(router_setting):
         assert False, 'Rate too low'
     time.sleep(5)
 
-
+@pytest.mark.dependency(depends=["connect"])
 @pytest.mark.wifi_connect
 def test_multi_throughtput_rx(router_setting):
     router_info = router_setting
--- tools/router_tool/router_performance.py
+++ tools/router_tool/router_performance.py
@@ -17,42 +17,16 @@ import json
 from util.mixin import json_mixin
 
 fpga = {
-    'w1': {'mino': '1x1', '2.4G': '11N', '5G': '11AC'},
-    'w1l': {'mino': '1x1', '2.4G': '11N', '5G': '11AC'},
-    'w2': {'mino': '2x2', '2.4G': '11AX', '5G': '11AX'},
-    'w2u': {'mino': '2x2', '2.4G': '11AX', '5G': '11AX'}
+    'w1': {'mimo': '1x1', '2.4G': '11N', '5G': '11AC'},
+    'w1l': {'mimo': '1x1', '2.4G': '11N', '5G': '11AC'},
+    'w2': {'mimo': '2x2', '2.4G': '11AX', '5G': '11AX'},
+    'w2u': {'mimo': '2x2', '2.4G': '11AX', '5G': '11AX'}
 }
-
+dut_fpgs = 'w2'
 
 def nested_defaultdict():
     return defaultdict(nested_defaultdict)
 
-@dataclass
-class router_info:
-    band: str
-    mode: str
-    bandwidth: str
-    mimo: str
-    direction: str
-    expect_data: int
-
-    def __post_init__(self):
-        if self.band.upper() not in ['2.4G', '5G', '6G']:
-            raise ValueError("The band can only be set to '2.4G','5G','6G' ")
-        if self.band.upper() == '2.4G':
-            if self.mode.upper() not in ["11N", '11AX']:
-                raise ValueError("2.4G can only be set to '11N' or '11AX' ")
-            if self.bandwidth not in ['20/40MHz', '20MHz', '40MHz']:
-                raise ValueError("2.4G can only be set to '20/40MHz', '20MHz', '40MHz' ")
-        elif self.band.upper() == '5G':
-            if self.mode.upper() not in ["11AX", "11AC"]:
-                raise ValueError("5G can only be set to '11AC' or '11AX' ")
-            if self.bandwidth not in ['20/40/80 MHz', '20MHz', '40MHz', '80MHz']:
-                raise ValueError("5G can only be set to '20/40/80MHz', '20MHz', '40MHz', '80MHz' ")
-        if self.mimo not in ['1x1', '2x2', '3x3', '4x4']:
-            raise ValueError("The mimo can only be set to '1x1','2x2','3x3','4x4'")
-        if self.direction.upper() not in ['UL', 'DL']:
-            raise ValueError("The direction can only be set to 'UL','DL'")
 
 @dataclass
 class compatibility_data(json_mixin):
@@ -73,36 +47,50 @@ class compatibility_data(json_mixin):
         self.model = self.model.upper()
         compatibility_data._instances.append(self)
 
-    def set_expect(self, info: router_info):
-        self.data[info.band][info.mode][info.bandwidth][info.mimo][info.direction] = info.expect_data
+    def set_expect(self, band, mode, bandwidth, mimo, direction, expect_data):
+        if band.upper() not in ['2.4G', '5G', '6G']:
+            raise ValueError("The band can only be set to '2.4G','5G','6G' ")
+        if band.upper() == '2.4G':
+            if mode.upper() not in ["11N", '11AX']:
+                raise ValueError("2.4G can only be set to '11N' or '11AX' ")
+            if bandwidth not in ['20/40MHz', '20MHz', '40MHz']:
+                raise ValueError("2.4G can only be set to '20/40MHz', '20MHz', '40MHz' ")
+        elif band.upper() == '5G':
+            if mode.upper() not in ["11AX", "11AC"]:
+                raise ValueError("5G can only be set to '11AC' or '11AX' ")
+            if bandwidth not in ['20/40/80 MHz', '20MHz', '40MHz', '80MHz']:
+                raise ValueError("5G can only be set to '20/40/80MHz', '20MHz', '40MHz', '80MHz' ")
+        if mimo not in ['1x1', '2x2', '3x3', '4x4']:
+            raise ValueError("The mimo can only be set to '1x1','2x2','3x3','4x4'")
+        if direction.upper() not in ['UL', 'DL']:
+            raise ValueError("The direction can only be set to 'UL','DL'")
+        self.band = band
+        self.mode = mode
+        self.bandwidth = bandwidth
+        self.mimo = mimo
+        self.direction = direction
+        self.expect_data = expect_data
+
+        self.data[band][mode][bandwidth][mimo][direction] = expect_data
 
     @classmethod
     def save_expect(cls):
         info = []
         for i in cls._instances:
+            print(i.to_dict())
             info.append(i.to_dict())
-        with open(f"{os.getcwd()}/config/compatobility_expectdata.json", 'w', encoding='utf-8') as f:
+        with open(f"compatobility_expectdata.json", 'w', encoding='utf-8') as f:
             json.dump(info, f, indent=4, ensure_ascii=False)
 
+
 a = compatibility_data("192.168.200.1", '1', 'asus', '88u')
-a.set_expect(router_info('2.4G', '11N', '40MHz', '1x1', 'UL', 400))
-a.set_expect(router_info('2.4G', '11N', '40MHz', '1x1', 'DL', 380))
-a.set_expect(router_info('5G', '11AX', '40MHz', '2x2', 'UL', 410))
-a.set_expect(router_info('5G', '11AX', '40MHz', '2x2', 'DL', 375))
+a.set_expect('2.4G', '11AX', '40MHz', '2x2', 'UL', 400)
+a.set_expect('2.4G', '11AX', '40MHz', '2x2', 'DL', 380)
+a.set_expect('5G', '11AX', '40MHz', '2x2', 'UL', 410)
+a.set_expect('5G', '11AX', '40MHz', '2x2', 'DL', 375)
 
 b = compatibility_data("192.168.200.2", '1', 'xiaomi', '5000')
-b.set_expect(router_info('2.4G', '11N', '40MHz', '1x1', 'DL', 110))
-b.set_expect(router_info('5G', '11AC', '40MHz', '2x2', 'UL', 220))
+b.set_expect('2.4G', '11N', '40MHz', '1x1', 'DL', 110)
+b.set_expect('5G', '11AC', '40MHz', '2x2', 'UL', 220)
 
-# compatibility_data.save_expect()
-
-print(compatibility_data._instances)
-print(filter(lambda x: x.ip == "192.168.200.1" and x.port =="1", compatibility_data._instances))
-def handle_expectdata(ip, port):
-    with open('config/compatobility_expectdata.json', 'r') as f:
-        datas = json.load(f)
-    for data in datas:
-        if data['ip'] == ip and data['port'] == port:
-            print(data)
-            return data['data']['2.4G'][fpga['w1']['2.4G']]['40MHz'][fpga['w1']['mino']]['UL']
-# print(handle_expectdata('192.168.200.1', '1'))
+compatibility_data.save_expect()
