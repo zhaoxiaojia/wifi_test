90056964672c657280e5b825914efa6f2741344f
zhaoxiaojia <799833928@qq.com>
2025-08-19 21:02:14 +0800

    调整极限用例逻辑

--- src/test/performance/test_wifi_peak_throughput.py
+++ src/test/performance/test_wifi_peak_throughput.py
@@ -23,8 +23,6 @@ router = get_router(router_name)
 logging.info(f'router {router}')
 test_data = get_testdata(router)
 
-step_list = [0]
-
 
 @pytest.fixture(scope='session', params=test_data, ids=[str(i) for i in test_data])
 def setup_router(request):
@@ -35,31 +33,22 @@ def setup_router(request):
     finally:
         next(common, None)
 
-
-@pytest.mark.parametrize("rf_value", step_list)
-def test_rvr(setup, rf_value):
-    if not setup[0]:
+def test_rvr(setup_router):
+    connect_status, router_info = setup_router
+    if not connect_status:
         logging.info("Can't connect wifi ,input 0")
         with open(pytest.testResult.detail_file, 'a') as f:
             f.write("\n Can't connect wifi , skip this loop\n\n")
         return
-    router_info = setup[1]
-
-    logging.info(f'rf_value {rf_value}')
 
     with open(pytest.testResult.detail_file, 'a') as f:
         f.write('-' * 40 + '\n')
-        info = ''
-        db_set = 0
-        info += 'db_set : \n'
-        info += 'corner_set : \n'
-        f.write(info)
 
     pytest.dut.get_rssi()
     logging.info('start test tx/rx')
     if int(router_info.tx):
         logging.info(f'rssi : {pytest.dut.rssi_num}')
-        pytest.dut.get_tx_rate(router_info, 'TCP', db_set=db_set)
+        pytest.dut.get_tx_rate(router_info, 'TCP')
     if int(router_info.rx):
         logging.info(f'rssi : {pytest.dut.rssi_num}')
-        pytest.dut.get_rx_rate(router_info, 'TCP', db_set=db_set)
+        pytest.dut.get_rx_rate(router_info, 'TCP')
