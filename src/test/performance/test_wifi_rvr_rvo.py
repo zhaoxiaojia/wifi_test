62971100a2eca6fb4779254e3af15ed1bff5033a
zhaoxiaojia <799833928@qq.com>
2025-08-19 20:07:06 +0800

    Update test_wifi_rvr_rvo.py

--- src/test/performance/test_wifi_rvr_rvo.py
+++ src/test/performance/test_wifi_rvr_rvo.py
@@ -27,7 +27,7 @@ test_data = get_testdata(router)
 
 
 @pytest.fixture(scope='session', params=test_data, ids=[str(i) for i in test_data])
-def setup(request):
+def setup_router(request):
     step_list = []
     rf_tool = None
     corner_tool = None
