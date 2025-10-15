91ef48f904542b27a20612570db048e149974995
zhaoxiaojia <799833928@qq.com>
2025-10-15 10:43:36 +0800

    移除 csv Data_RSSI 属性

--- src/tools/TestResult.py
+++ src/tools/TestResult.py
@@ -51,25 +51,11 @@ class TestResult():
                 title = (
                     'SerianNumber Test_Category Standard Freq_Band BW Data_Rate '
                     'CH_Freq_MHz Protocol Direction Total_Path_Loss DB RSSI Angel '
-                    'Data_RSSI MCS_Rate Throughput Expect_Rate '
+                    'MCS_Rate Throughput Expect_Rate '
                 )
                 f.write(','.join(title.split()))
                 f.write('\n')
 
-        # with open(os.path.join(os.getcwd(),
-        #                        'config\\rvr_wifi_setup.csv' if pytest.win_flag else 'config/rvr_wifi_setup.csv'),
-        #           'r', encoding='utf-8') as f:
-        #     reader = csv.reader(f)
-        #     self.results_length = []
-        #     for i in [j for j in reader][1:]:
-        #         if not i:
-        #             continue
-        #         temp = 0
-        #         if 'tx' in i[-2]:
-        #             temp += self.x_length
-        #         if 'rx' in i[-2]:
-        #             temp += self.x_length
-        #         self.results_length.append(temp)
 
     def save_result(self, result):
         '''
@@ -84,151 +70,6 @@ class TestResult():
             f.write('\n')
         logging.info('Write done')
 
-    def write_to_excel(self):
-        '''
-        format csv to excel
-        @return: None
-        '''
-        logging.info('Write to excel')
-
-        df = pd.read_csv(self.log_file, encoding='utf-8')
-        # 转置数据
-        # df = pd.DataFrame(df.values.T, index=df.columns, columns=df.index)
-        if not os.path.exists(self.rvr_excelfile):
-            df.to_excel(self.rvr_excelfile, sheet_name=time.asctime().replace(' ', '_').replace(':', '-'))
-        else:
-            with pd.ExcelWriter(self.rvr_excelfile, engine='openpyxl', mode='a') as f:
-                df.to_excel(f, sheet_name=time.asctime().replace(' ', '_').replace(':', '-'))
-        logging.info('Write done')
-
-    def write_attenuation_data_to_pdf(self):
-        '''
-        format excel to pdf
-        @return: None
-        '''''
-        logging.info('Write to pdf')
-        with PdfPages(os.path.join(self.logdir, 'RvrResult.pdf')) as rvr_pdffile:
-            df = pd.read_excel(self.rvr_excelfile, sheet_name=None)
-            all_data = []
-            # print(list(df.keys()))
-            # ['Fri_Feb_11_16-44-19_2022', 'Fri_Feb_11_16-45-16_2022', 'Fri_Feb_11_16-45-49_2022']
-            io = pd.io.excel.ExcelFile(self.rvr_excelfile)
-            for i in list(df.keys()):
-                all_data.append(pd.read_excel(io, sheet_name=i).values)
-
-            io.close()
-
-            # title = all_data[0][:, 0].tolist()
-            all_data = [i.tolist() for i in all_data][0]
-            logging.info(f'all_data {all_data}')
-            logging.info(f'results_length {self.results_length}')
-            temp = 0
-            chart_date = []
-            for i in self.results_length:
-                chart_date.append(all_data[temp:temp + i])
-                temp = temp + i
-            logging.info(f'chart_data {chart_date}')
-            # for i in chart_date:
-            #     print(i)
-            #     print(len(i))
-            plt.figure(figsize=(10, 10))
-            plt.suptitle("Rvr test report summary")
-            plt.subplots_adjust(wspace=0.3, hspace=0.8)
-
-            for i in chart_date:
-                ax = plt.subplot(5, 2, 1 + chart_date.index(i))
-                logging.info(f'i {i}')
-                # [0, 'P0', 'RvR', 'Standalone', nan, 'Null', '11AX', 5.0, 80, 'Rate_Adaptation', 149, 'TCP', 'UL', nan, nan, -21, nan, 90, 'msc_tx']
-                ax.set_title(f'{i[0][7]}_{i[0][6]}_{i[0][8]}_{i[0][10]}')
-                # print(i)
-                tx_results = []
-                rx_results = []
-                for j in i:
-                    # print(j)
-                    if 'UL' in j[-8]:
-                        if j[-2] == 'False':
-                            logging.warning('date wrong')
-                            tx_results.append(0)
-                        else:
-                            tx_results.append(int(j[-2]))
-                    else:
-                        if j[-2] == 'False':
-                            logging.warning('data wrong')
-                            rx_results.append(0)
-                        else:
-                            rx_results.append(int(j[-2]))
-                # print(rx_results)
-                # print(tx_results)
-                if tx_results:
-                    plt.plot(self.x_path, tx_results, label='tx')
-                if rx_results:
-                    plt.plot(self.x_path, rx_results, label='rx')
-                # plt.plot(self.x_path, [100 for i in range(len(self.x_path))], label='Singal Debility')
-                plt.xlabel("Attenuation")
-                plt.ylabel("Throughput rate Mb/s")
-                plt.legend()
-            # plt.show()
-            rvr_pdffile.savefig()
-
-    def write_corner_data_to_pdf(self):
-        '''
-        format excel to pdf
-        @return: None
-        '''''
-        logging.info('Write to pdf')
-        with PdfPages(os.path.join(self.logdir, 'RvrCornerResult.pdf')) as rvr_pdffile:
-            df = pd.read_excel(self.rvr_excelfile, sheet_name=None)
-            all_data = []
-            io = pd.io.excel.ExcelFile(self.rvr_excelfile)
-            for i in list(df.keys()):
-                all_data.append(pd.read_excel(io, sheet_name=i).values)
-            io.close()
-            # title = all_data[0][:, 0].tolist()
-            all_data = [i.tolist() for i in all_data][0]
-            temp = 0
-            chart_date = []
-            for i in self.results_length:
-                chart_date.append(all_data[temp:temp + i])
-                temp = temp + i
-            fig = plt.figure(figsize=(20, 20), dpi=100)
-            fig.tight_layout()
-            plt.suptitle("Rvr test report summary")
-            plt.subplots_adjust(wspace=0, hspace=0.3)
-            angles = np.linspace(0, 2 * np.pi, len(self.x_path), endpoint=False)
-            angles = np.concatenate((angles, [angles[0]]))
-            feature = np.concatenate((self.x_path, [self.x_path[0]]))
-            for i in chart_date:
-                ax = fig.add_subplot(2, 3, 1 + chart_date.index(i), polar=True)
-                # print(i)
-                # [0, 'P0', 'RvR', 'Standalone', nan, 'Null', '11AX', 5.0, 80, 'Rate_Adaptation', 149, 'TCP', 'UL', nan, nan, -21, nan, 90, 'msc_tx']
-                ax.set_title(f'{i[0][7]}_{i[0][6]}_{i[0][8]}_{i[0][10]}')
-                tx_results, rx_results = [], []
-                for j in i:
-                    if 'UL' in j[-8]:
-                        if j[-2] == 'False':
-                            logging.warning('data wrong')
-                            tx_results.append(0)
-                        else:
-                            tx_results.append(int(j[-2]))
-                    else:
-                        if j[-2] == 'False':
-                            logging.warning('data wrong')
-                            rx_results.append(0)
-                        else:
-                            rx_results.append(int(j[-2]))
-                if tx_results:
-                    tx_results = np.concatenate((tx_results, [tx_results[0]]))
-                    ax.plot(angles, tx_results, lw=0.5, label='tx')
-                if rx_results:
-                    rx_results = np.concatenate((rx_results, [rx_results[0]]))
-                    ax.plot(angles, rx_results, lw=0.5, label='rx')
-                ax.set_thetagrids(angles * 180 / np.pi, feature)
-                ax.set_theta_zero_location('N')
-                # ax.set_ylim(0,400)
-                ax.set_rlabel_position(0)
-                plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=1, frameon=False, )
-            # plt.show()
-            rvr_pdffile.savefig()
 
 # rvr = RvrResult('D:\windows RVR\wins_wifi',step=[i for i in range(0,40,2)])
 # rvr.write_to_excel()
